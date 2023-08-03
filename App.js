fb5c1a0a913007503a2296fb9aa1464ff45e317c
Michael <michaelolivares123@gmail.com>
2023-08-03 10:47:51 -0500

    fix(): one component banner with one design and orientation

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import BannerVertical from './src/components/banner/bannerVertical';
-import BannerHorizontal from './src/components/banner/bannerHorizontal';
-import {useOrientation} from './src/hooks/useOrientation';
 import videoSample from './src/assets/video/video2.mp4';
+import Banner from './src/components/banner/banner';
 
 function App() {
-  const orientation = useOrientation();
-  return orientation === 'PORTRAIT' ? (
-    <BannerVertical
-      img1={
-        'https://cdn.pixabay.com/photo/2017/04/26/10/33/tram-2262256_640.jpg'
-      }
-      img2={
-        'https://cdn.pixabay.com/photo/2019/03/10/18/31/hong-kong-4046913_1280.jpg'
-      }
-      img3={
-        'https://cdn.pixabay.com/photo/2016/11/29/10/01/vw-bulli-1868890_640.jpg'
-      }
-      text={
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-      }
-      video={videoSample}
-    />
-  ) : (
-    <BannerHorizontal
+  return (
+    <Banner
       img1={
         'https://cdn.pixabay.com/photo/2017/04/26/10/33/tram-2262256_640.jpg'
       }
---
+++ src/components/banner/banner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BannerHorizontal from './bannerHorizontal';
+import BannerVertical from './bannerVertical';
+import {useOrientation} from '../../hooks/useOrientation';
+
+const Banner = ({img1, img2, img3, video, text}) => {
+  const orientation = useOrientation();
+  return orientation === 'PORTRAIT' ? (
+    <BannerVertical
+      img1={img1}
+      img2={img2}
+      img3={img3}
+      text={text}
+      video={video}
+    />
+  ) : (
+    <BannerHorizontal
+      img1={img1}
+      img2={img2}
+      img3={img3}
+      text={text}
+      video={video}
+    />
+  );
+};
+
+export default Banner;
